123e6fb8a731080aca1c537816de5540ebcc154b
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-26 03:14:49 +0300

    php promise8

--- index.php
+++ index.php
@@ -3,13 +3,27 @@
     ini_set('display_errors', 1);
     require 'vendor/autoload.php';
     use GuzzleHttp\Client;
-
+    use GuzzleHttp\Psr7\Request;
+    use Psr\Http\Message\ResponseInterface;
+    use GuzzleHttp\Exception\RequestException;
+    
     $name = $_GET['name'] ?? 'Herokuist';
     $client = new Client([
         'base_uri' => 'https://kodaktor.ru',
         'timeout'  => 2.0,
     ]);
-    $response = $client->get('/api/req?name=' . $name);
+    $headers = ['Clinet' => 'Elias@Heroku'];
+    $body = 'Hello!';
+    $request = new Request('POST', '/api/req?name=' . $name, $headers, $body);
+    $promise = $client->requestAsync($request);
+    $promise->then(
+        function (ResponseInterface $res) {
+            $result = json_decode($response ->getBody());
+            echo '<h2>' . $result -> name . '</h2>'; 
+        },
+        function (RequestException $e) {
+            echo $e->getMessage() . "\n";
+            echo $e->getRequest()->getMethod();
+        }
+    );
     echo '<h1>Promises</h1>';
-    $result = json_decode($response ->getBody());
-    echo '<h2>' . $result -> name . '</h2>'; 
