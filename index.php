e31361300f48b3bc3ce520f8424c25c4518d0ec0
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-26 03:02:03 +0300

    php promise6

--- index.php
+++ index.php
@@ -8,19 +8,6 @@
         'timeout'  => 2.0,
     ]);
     $response = $client->get('/api/req?name=Heroku');
-    /*
-    use Psr\Http\Message\ResponseInterface;
-    use GuzzleHttp\Exception\RequestException;
-    $promise = $client->requestAsync('GET', '/api/req?name=Heroku');
-    $promise->then(
-        function (ResponseInterface $res) {
-            echo $res->getBody() . "\n";
-        },
-        function (RequestException $e) {
-            echo $e->getMessage() . "\n";
-            echo $e->getRequest()->getMethod();
-        }
-    );
-    */
     echo '<h1>Promises</h1>';
-    echo $response ->getBody();
+    $result = json_decode($response ->getBody());
+    echo '<h2>' . $result -> name . '</h2>'; // Heroku
