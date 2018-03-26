4726a0de1b3872e61fecf506c88ff46331d35994
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-26 03:07:16 +0300

    php promise7

--- index.php
+++ index.php
@@ -3,11 +3,13 @@
     ini_set('display_errors', 1);
     require 'vendor/autoload.php';
     use GuzzleHttp\Client;
+
+    $name = $_GET['name'] ?? 'Herokuist';
     $client = new Client([
         'base_uri' => 'https://kodaktor.ru',
         'timeout'  => 2.0,
     ]);
-    $response = $client->get('/api/req?name=Heroku');
+    $response = $client->get('/api/req?name=' . $name);
     echo '<h1>Promises</h1>';
     $result = json_decode($response ->getBody());
-    echo '<h2>' . $result -> name . '</h2>'; // Heroku
+    echo '<h2>' . $result -> name . '</h2>'; 
