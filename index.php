d6f801284523a745b2dac36e15d323fb79fe6aae
gossoudarev <igossoudarev@herzen.spb.ru>
2018-03-26 03:37:17 +0300

    php promise14

--- index.php
+++ index.php
@@ -13,14 +13,14 @@
         'timeout'  => 2.0,
     ]);
     $headers = ['Client' => 'Elias@Heroku'];
-    $body = 'Hello!';
-    // $promise = new Request('POST', 'https://kodaktor.ru/api/req?name=' . $name, $headers, $body);
+    $body = 'name=' . $name;
+    // $promise = new Request('POST', 'https://kodaktor.ru/api/req', $headers, $body);
     // $promise = $client->requestAsync($request);
     // $promise = $client->requestAsync('POST', 'https://kodaktor.ru/api/req?name=' . $name);
     $promise = $client->postAsync('/api/req?name=' . $name, $headers, $body);
     $promise->then(
         function (ResponseInterface $res) {
-            $result = json_decode($response ->getBody());
+            $result = json_decode($res ->getBody());
             echo '<h2>' . $result -> name . '</h2>'; 
         },
         function (RequestException $e) {
