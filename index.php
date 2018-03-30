920c490626874fdc32b9f1da2d133d3d3eeb2dd9
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-03-30 08:08:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-    error_reporting(E_ALL);
-    ini_set('display_errors', 1);
     require 'vendor/autoload.php';
     use GuzzleHttp\Client;
     $URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20woeid%3D%222123260%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"; 
