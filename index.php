1c629e4fbf3b7d911c2d09078dc16b4274e0e1a3
Ilya Goss <igossoudarev@herzen.spb.ru>
2018-03-30 08:07:28 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,9 @@
     ini_set('display_errors', 1);
     require 'vendor/autoload.php';
     use GuzzleHttp\Client;
-
-    $name = $_GET['name'] ?? 'Herokuist';
-    $client = new Client([
-        'base_uri' => 'https://kodaktor.ru',
-        'timeout'  => 2.0,
-    ]);
-    $response = $client->get('/api/req?name=' . $name);
+    $URL = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20woeid%3D%222123260%22)%20and%20u%3D'c'&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"; 
+    $client = new Client();
+    $response = $client->get($URL);
     echo '<h1>Promises</h1>';
     $result = json_decode($response ->getBody());
-    echo '<h2>' . $result -> name . '</h2>'; 
+    echo '<h2>' . $result -> query -> results -> channel -> item -> forecast[1] -> low  . '</h2>';
